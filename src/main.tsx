900e1042ea9c05f521c361178a4db0c08e12b2f8
Amransenan10 <amransenan2016@gmail.com>
2026-03-03 22:30:22 +0300

    fix build issue with vite resolution on vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
-import { CashierPage } from './pages/CashierPage.tsx';
+import App from './App';
+import { CashierPage } from './pages/CashierPage';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
